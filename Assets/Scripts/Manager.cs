4d88eca8c9afc4e1e736acc32fabcfa28aa2c282
Frostlag <dingtaoyin@hotmail.com>
2015-05-29 19:22:30 -0400

    Tank and Manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	public Tank tank;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("left")) {
+			tank.SendMessage("Move","left");
+		}
+		if (Input.GetKey ("right")) {
+			tank.SendMessage("Move","right");
+		} 
+	}
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+	Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Move(string direction){
+		if (direction == "left") {
+			Debug.Log("left");
+			rigidBody.AddForce (transform.right.normalized * -1);
+		} else {
+			Debug.Log("right");
+			rigidBody.AddForce (transform.right.normalized * 1);
+		}
+	}
+}
