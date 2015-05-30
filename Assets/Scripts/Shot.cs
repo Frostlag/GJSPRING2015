2b70baf5e2b8cd8e77958fe724bb129d44264e34
Frostlag <dingtaoyin@hotmail.com>
2015-05-29 20:56:48 -0400

    Stage

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Shot : MonoBehaviour {
 	private float startTime;
-	public int life;
+	public float life;
 	// Use this for initialization
 	void Start () {
 		startTime = Time.time;
@@ -11,6 +11,8 @@ public class Shot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Time.time > startTime + life) {
+			Destroy(gameObject);
+		}
 	}
 }
