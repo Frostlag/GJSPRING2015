a0a39f629b9890d5b9c300e14c627da82c17cdf4
Frostlag <dingtaoyin@hotmail.com>
2015-05-31 13:00:57 -0400

    spash

---
+++ Assets/Scripts/Spash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect (Screen.width/2-50,Screen.height/2,100,20), "Click")) {
+			Manager.begin = true;
+			Destroy (gameObject);
+		}
+
+	}
+}
